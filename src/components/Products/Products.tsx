ede06a11dc575ed0a51590a63420c92c04569dc4
badano-nicolas <badano.nicolas@gmail.com>
2022-05-25 23:14:49 -0300

    Styled products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import data from "../../lib/products.json";
+import "./Products.scss";
 
 const Products = () => {
   return (
-    <div>
+    <div className="products-container">
       {data.products.map((product, i) => (
-        <div key={i}>
+        <div key={i} className="product">
           <h3>{product.name} </h3>
           <p>Precio: ${product.price}</p>
           <p>Restantes: {product.amount}</p>
+          <button onClick={() => console.log(product)}>Agregar al carro</button>
         </div>
       ))}
     </div>
